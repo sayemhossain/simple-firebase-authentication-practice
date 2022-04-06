c4a260501b6bcd415dd98d65f911a56a895d5bd4
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-07 03:05:47 +0600

    login logout button solve

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 function App() {
+  const [user, setUser] = useState();
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch((error) => console.log(error));
   };
-
+  const handleLogOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => setUser({}));
+  };
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>google signIn</button>
+      {user.email ? (
+        <button onClick={handleLogOut}>Logout</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google signIn</button>
+      )}
+      <h3>Name: {user.displayName}</h3>
+      <p>Gmail: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
