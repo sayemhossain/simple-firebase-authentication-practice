9246dfc5d6d6ca4700460b3cefafdcc8f403fe76
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-08 21:38:37 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import {
   signOut,
 } from "firebase/auth";
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import { Button, Form } from "react-bootstrap";
 
 const auth = getAuth(app);
 function App() {
@@ -64,32 +67,53 @@ function App() {
     e.preventDefault();
   };
   return (
-    <div className="App">
-      {user.uid ? (
-        <button onClick={handleLogOut}>Logout</button>
-      ) : (
-        <>
-          <button onClick={handleGoogleSignIn}>Google signIn</button>
-          <button onClick={handleGithubSignIn}>Github signIn</button>
-        </>
-      )}
+    <div>
+      <div className="text-center my-5">
+        {user.uid ? (
+          <button onClick={handleLogOut}>Logout</button>
+        ) : (
+          <>
+            <button onClick={handleGoogleSignIn}>Google signIn</button>
+            <button onClick={handleGithubSignIn}>Github signIn</button>
+          </>
+        )}
 
-      <div>
-        <form onSubmit={handleFormSubmit}>
-          <input onBlur={handleEmailBlur} type="email" name="email" id="" />
-          <input
-            onBlur={handlePasswordBlur}
-            type="password"
-            name="password"
-            id=""
-          />
-          <input type="submit" value="Submit" />
-        </form>
+        <h3>Name: {user.displayName}</h3>
+        <p>Gmail: {user.email}</p>
+        <img src={user.photoURL} alt="" />
       </div>
 
-      <h3>Name: {user.displayName}</h3>
-      <p>Gmail: {user.email}</p>
-      <img src={user.photoURL} alt="" />
+      <div className="mx-auto bg-success w-50">
+        <h2 className="text-center pt-3">Registration Form</h2>
+        <Form onSubmit={handleFormSubmit} className=" mx-auto  p-5">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              onBlur={handleEmailBlur}
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onBlur={handlePasswordBlur}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
